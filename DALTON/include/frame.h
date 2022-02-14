bd9d1e6a492103546e85e1078d439d2c1bb2e12a
Hans Jørgen Aa. Jensen <hjj@sdu.dk>
2022-02-14 19:06:09 +0100

    hjaaj. Minor changes in comments and output.
    + corrected  calculation of nuclear part of dipole moment in DPNUC(3)
      (currently not used anywhere ...)

--- DALTON/include/frame.h
+++ DALTON/include/frame.h
@@ -1,1 +1,5 @@
+! frame.h
+!  POTNUC : nuclear repulsion energy
+!  DPNUC  : nuclear dipole moment for origin (0,0,0)
+!  QPNUC  : nuclear quadrupole moment for origin (0,0,0)
       COMMON /FRAME/ POTNUC, DPNUC(3), QPNUC(6)
