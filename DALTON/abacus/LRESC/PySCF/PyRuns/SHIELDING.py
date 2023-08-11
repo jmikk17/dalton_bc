987d8195f3ef4b778823e02b8498fdfb5f64f419
juanjoaucar <juanaucar@gmail.com>
2023-08-10 23:14:39 -0300

    First results for new FC type integrals - LRESC shielding

--- DALTON/abacus/LRESC/PySCF/PyRuns/SHIELDING.py
+++ DALTON/abacus/LRESC/PySCF/PyRuns/SHIELDING.py
@@ -2,6 +2,7 @@ import pyscf
 from pyscf import gto,scf
 import numpy
 
+# (r_i . nabla_j) / (r^3)
 def _get_DIFC_I(mol, atm_id, cartesian = False ):
     nao_sph = mol.intor('int1e_ovlp_sph').shape[0]
     nao_cart = mol.intor('int1e_ovlp_cart').shape[0]
@@ -18,10 +19,15 @@ def _get_DIFC_I(mol, atm_id, cartesian = False ):
             ipiprinv = mol.intor('int1e_ipiprinv_sph', 9).reshape(3,3,nao_sph,nao_sph)
             rinvipip=numpy.transpose(ipiprinv,axes=[0,1,3,2]).conjugate()
 
-    integrals=iprinvip+ipiprinv
+    integrals=iprinvip+rinvipip #ipiprinv
     return integrals
 
+# (r . nabla) / (r^3)
+def _get_DIFC_0(mol, atm_id, cartesian = False ):
+    DIFC_I = _get_DIFC_I(mol, atm_id, cartesian)
+    return DIFC_I[0,0,:,:]+DIFC_I[1,1,:,:]+DIFC_I[2,2,:,:]
 
+# (r_i r_j)/(r^5) r . nabla
 def _get_DIFC_II(mol, atm_id, cartesian = False ):
     nao_sph = mol.intor('int1e_ovlp_sph').shape[0]
     nao_cart = mol.intor('int1e_ovlp_cart').shape[0]
@@ -47,26 +53,30 @@ def _get_DIFC_II(mol, atm_id, cartesian = False ):
             integrals=numpy.zeros([3,3,nao_sph,nao_sph])
 
     for k in range(3):
-        integrals = integrals + iprinviprip[:,:,k,k,:,:] + numpy.transpose(iprinviprip[:,:,k,k,:,:],axes=[1,0,2,3]) + numpy.transpose(rinvipiprip[:,:,k,k,:,:],axes=[1,0,2,3]) + numpy.transpose(ipiprinvrip[:,:,k,k,:,:],axes=[1,0,2,3]) +iprinvip[k,k,:,:] + rinvipip[k,k,:,:]
+        integrals = integrals + iprinviprip[:,:,k,k,:,:] + numpy.transpose(iprinviprip[:,:,k,k,:,:],axes=[1,0,2,3]) + numpy.transpose(rinvipiprip[:,:,k,k,:,:],axes=[1,0,2,3]) + numpy.transpose(ipiprinvrip[:,:,k,k,:,:],axes=[1,0,2,3]) #+ iprinvip[k,k,:,:] + rinvipip[k,k,:,:]
 
+    for k in range(3):
+        integrals[0,0,:,:] = integrals[0,0,:,:] + iprinvip[k,k,:,:] + rinvipip[k,k,:,:]
+        integrals[1,1,:,:] = integrals[1,1,:,:] + iprinvip[k,k,:,:] + rinvipip[k,k,:,:]
+        integrals[2,2,:,:] = integrals[2,2,:,:] + iprinvip[k,k,:,:] + rinvipip[k,k,:,:]
 
     return integrals/3
 
 
 
 
-base = "dyall_cv2z"
+base = "dyall_cv4z"
 
 mol_h2o = gto.M(atom="molecula.xyz",basis = base)
 mol_h2o, ctr_coeff = mol_h2o.to_uncontracted_cartesian_basis()
 
 naos_sph = mol_h2o.intor('int1e_ovlp_sph').shape[0]
 nao_cart = mol_h2o.intor('int1e_ovlp_cart').shape[0]
-nro_operadores=numpy.array([18*mol_h2o.natm])
+nro_operadores=numpy.array([19*mol_h2o.natm])
 print("naos_cart:",nao_cart)
 print("naos_sph:",naos_sph)
 print("nro de operadores a grabar: ",nro_operadores)
-cartesian = True
+cartesian = False
 if (cartesian):
    print("Cartesian basis set used at PySCF")
 else:
@@ -83,52 +93,57 @@ with open("shi.bin", "wb") as file:
 
 
 for i in range(mol_h2o.natm):
-    DIFC_I=_get_DIFC_I(mol_h2o, atm_id = i, cartesian=True)
-    titulos1_row1=['FC01xx'+"{:02.0f}".format(i+1), 'FC01xy'+"{:02.0f}".format(i+1), 'FC01xz'+"{:02.0f}".format(i+1)]
-    titulos1_row2=['FC01yx'+"{:02.0f}".format(i+1), 'FC01yy'+"{:02.0f}".format(i+1), 'FC01yz'+"{:02.0f}".format(i+1)]
-    titulos1_row3=['FC01zx'+"{:02.0f}".format(i+1), 'FC01zy'+"{:02.0f}".format(i+1), 'FC01zz'+"{:02.0f}".format(i+1)]
+    DIFC_0=_get_DIFC_0(mol_h2o, atm_id = i, cartesian = cartesian)
+    titulos1=['FC1  '+"{:03.0f}".format(i+1)]
 
-    DIFC_II=_get_DIFC_II(mol_h2o, atm_id = i, cartesian=True)
-    titulos2_row1=['FC02xx'+"{:02.0f}".format(i+1), 'FC02xy'+"{:02.0f}".format(i+1), 'FC02xz'+"{:02.0f}".format(i+1)]
-    titulos2_row2=['FC02yx'+"{:02.0f}".format(i+1), 'FC02yy'+"{:02.0f}".format(i+1), 'FC02yz'+"{:02.0f}".format(i+1)]
-    titulos2_row3=['FC02zx'+"{:02.0f}".format(i+1), 'FC02zy'+"{:02.0f}".format(i+1), 'FC02zz'+"{:02.0f}".format(i+1)]
+    DIFC_I=_get_DIFC_I(mol_h2o, atm_id = i, cartesian = cartesian)
+    titulos2_row1=['FC2xx'+"{:03.0f}".format(i+1), 'FC2xy'+"{:03.0f}".format(i+1), 'FC2xz'+"{:03.0f}".format(i+1)]
+    titulos2_row2=['FC2yx'+"{:03.0f}".format(i+1), 'FC2yy'+"{:03.0f}".format(i+1), 'FC2yz'+"{:03.0f}".format(i+1)]
+    titulos2_row3=['FC2zx'+"{:03.0f}".format(i+1), 'FC2zy'+"{:03.0f}".format(i+1), 'FC2zz'+"{:03.0f}".format(i+1)]
 
+    DIFC_II=_get_DIFC_II(mol_h2o, atm_id = i, cartesian = cartesian)
+    titulos3_row1=['FC3xx'+"{:03.0f}".format(i+1), 'FC3xy'+"{:03.0f}".format(i+1), 'FC3xz'+"{:03.0f}".format(i+1)]
+    titulos3_row2=['FC3yx'+"{:03.0f}".format(i+1), 'FC3yy'+"{:03.0f}".format(i+1), 'FC3yz'+"{:03.0f}".format(i+1)]
+    titulos3_row3=['FC3zx'+"{:03.0f}".format(i+1), 'FC3zy'+"{:03.0f}".format(i+1), 'FC3zz'+"{:03.0f}".format(i+1)]
 
     with open("shi.bin", "ab") as file:
-        file.write(titulos1_row1[0].encode('ascii'))
-        numpy.array(DIFC_I[0,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
-        file.write(titulos1_row1[1].encode('ascii'))
-        numpy.array(DIFC_I[0,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
-        file.write(titulos1_row1[2].encode('ascii'))
-        numpy.array(DIFC_I[0,2,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
-        file.write(titulos1_row2[0].encode('ascii'))
-        numpy.array(DIFC_I[1,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
-        file.write(titulos1_row2[1].encode('ascii'))
-        numpy.array(DIFC_I[1,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
-        file.write(titulos1_row2[2].encode('ascii'))
-        numpy.array(DIFC_I[1,2,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
-        file.write(titulos1_row3[0].encode('ascii'))
-        numpy.array(DIFC_I[2,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
-        file.write(titulos1_row3[1].encode('ascii'))
-        numpy.array(DIFC_I[2,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
-        file.write(titulos1_row3[2].encode('ascii'))
-        numpy.array(DIFC_I[2,2,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        file.write(titulos1[0].encode('ascii'))
+        numpy.array(DIFC_0[:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
 
         file.write(titulos2_row1[0].encode('ascii'))
-        numpy.array(DIFC_II[0,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        numpy.array(DIFC_I[0,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
         file.write(titulos2_row1[1].encode('ascii'))
-        numpy.array(DIFC_II[0,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        numpy.array(DIFC_I[0,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
         file.write(titulos2_row1[2].encode('ascii'))
-        numpy.array(DIFC_II[0,2,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        numpy.array(DIFC_I[0,2,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
         file.write(titulos2_row2[0].encode('ascii'))
-        numpy.array(DIFC_II[1,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        numpy.array(DIFC_I[1,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
         file.write(titulos2_row2[1].encode('ascii'))
-        numpy.array(DIFC_II[1,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        numpy.array(DIFC_I[1,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
         file.write(titulos2_row2[2].encode('ascii'))
-        numpy.array(DIFC_II[1,2,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        numpy.array(DIFC_I[1,2,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
         file.write(titulos2_row3[0].encode('ascii'))
-        numpy.array(DIFC_II[2,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        numpy.array(DIFC_I[2,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
         file.write(titulos2_row3[1].encode('ascii'))
-        numpy.array(DIFC_II[2,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        numpy.array(DIFC_I[2,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
         file.write(titulos2_row3[2].encode('ascii'))
+        numpy.array(DIFC_I[2,2,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+
+        file.write(titulos3_row1[0].encode('ascii'))
+        numpy.array(DIFC_II[0,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        file.write(titulos3_row1[1].encode('ascii'))
+        numpy.array(DIFC_II[0,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        file.write(titulos3_row1[2].encode('ascii'))
+        numpy.array(DIFC_II[0,2,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        file.write(titulos3_row2[0].encode('ascii'))
+        numpy.array(DIFC_II[1,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        file.write(titulos3_row2[1].encode('ascii'))
+        numpy.array(DIFC_II[1,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        file.write(titulos3_row2[2].encode('ascii'))
+        numpy.array(DIFC_II[1,2,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        file.write(titulos3_row3[0].encode('ascii'))
+        numpy.array(DIFC_II[2,0,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        file.write(titulos3_row3[1].encode('ascii'))
+        numpy.array(DIFC_II[2,1,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
+        file.write(titulos3_row3[2].encode('ascii'))
         numpy.array(DIFC_II[2,2,:,:], dtype=numpy.float64).transpose(1,0).tofile(file)
