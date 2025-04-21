4a109174e80f80a268fc5acc3563c173f23fc5ff
Hans Joergen Aagard Jensen <hjj@sdu.dk>
2025-04-21 21:01:56 +0200

    hjaaj. Turns out that MAXCHN and MAXADR refer to the same thing. Changed MAXADR to MAXCHN.
    - found out when looking into that rsp_zfs_mc2 test with increased basis set, aug-cc-pVTZ,
      lead to to many chains in PT1SOR.
    - have increased MAXCHN frokm 200 to 2000.

--- DALTON/abacus/ptrbuf.h
+++ DALTON/abacus/ptrbuf.h
@@ -1,13 +1,12 @@
 ! FILE: ptrbuf.h -- only used in DALTON/abacus/abaptr.F
 !                -- depends on: maxorb.h
-      INTEGER         MAXADR, MAXCHN
-      PARAMETER       (MAXADR = 200, MAXCHN = 200)
+      INTEGER, parameter :: MAXCHN = 2000
 
       INTEGER         LASTAD, IADR
       INTEGER         MX1BUF, L1BUF,  MEMS,   MEMT,  MXABUF,
      &                LABUF,  LDAMAX, MX2BUF, L2BUF, NCHAIN, NBLOCK
       COMMON /PTRBUF/
-     &                LASTAD(MAXADR), IADR(MAXADR),
+     &                LASTAD(MAXCHN), IADR(MAXCHN),
      &                MX1BUF, L1BUF,  MEMS,   MEMT,  MXABUF,
      &                LABUF,  LDAMAX, MX2BUF, L2BUF, NCHAIN, NBLOCK
 ! end of ptrbuf.h
